d27a1e752889b2746f98db6151e7bb94f6420c85
optXuDLi <lixudongkeer@gmail.com>
2020-10-01 08:20:02 +0800

    Delete startup.m

--- startup.m
+++
@@ -1,9 +0,0 @@
-%%
-  Home = pwd;
-  addpath(genpath(Home));
-%%
-%%
-  set(0,'defaultaxesfontsize',14);
-  set(0,'defaultlinemarkersize',6);
-  set(0,'defaulttextfontsize',14);
-%%*************************************************************************
